d2ef40e39fa1237a7d76d95c8911f042f89aca03
jhzcarvalho <jhcarvalho3@gmail.com>
2023-12-04 11:05:26 -0300

    primeiro commit

---
+++ caneta.java
@@ -0,0 +1,31 @@
+/**
+ * caneta
+ */
+public class caneta {
+
+    private String cor;
+    private Float peso;
+    private String marca;
+    private Integer carga;
+    private Float espessura;
+
+    public String getCor() {
+        return this.cor;
+    }
+
+    public Float getPeso() {
+        return this.peso;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public Integer getCarga() {
+        return this.carga;
+    }
+
+    public Float getEspessura() {
+        return this.espessura;
+    }
+}
